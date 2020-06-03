91f887e2677390230dc9d7265d43ab2c46258885
Your Name <you@example.com>
2020-06-03 16:13:04 +0100

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,91 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { faTwitterSquare } from "@fortawesome/free-brands-svg-icons";
+import { faTumblrSquare } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const quotes = [
+	{ 
+		quote: "Our species can only survive if we have obstacles to overcome. You remove those obstacles. Without them to strengthen us, we will weaken and die." ,
+	  	author: "Captain Kirk",
+	 },
+	{ 
+		quote: "Change is the essential process of all existence." ,
+	  	author: "Spock",
+	 },
+	{ 
+		quote: "With the first link, the chain is forged. The first speech censored, the first thought forbidden, the first freedom denied, chains us all irrevocably." ,
+	  	author: "Jean-Luc Picard",
+	 },
+	{ 
+		quote: "You may find that having is not so pleasing a thing as wanting. This is not logical, but it is often true." ,
+	  	author: "Spock",
+	 },
+	{ 
+		quote: "Live now; make now always the most precious time. Now will never come again." ,
+	  	author: "Jean-Luc Picard",
+	 },	 	 	 	 
+];
+
+
+function Social(props) {
+	return (
+		<a href={props.href} id="tweet-quote">
+			<FontAwesomeIcon icon={props.icon} />
+		</a>
+		);
+}
+
+class Quote extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<React.Fragment>
+				<div id="text">{quotes[this.props.index].quote}</div>
+				<div id="author">{quotes[this.props.index].author}</div>
+			</React.Fragment>
+			);
+	}
+
+}
+
+
+
+class RandomQuoteGenerator extends React.Component {
+	constructor(props) {
+		super(props);
+		this.handleClick = this.handleClick.bind(this);	
+	}
+
+	handleClick() {
+		const quoteLength = quotes.length;
+		console.log(quoteLength);
+	}
+
+	render() {
+		return (
+			<React.Fragment>
+				<div id="quote-box">
+					<Quote index={3}/>
+				</div>	
+				<div>
+					<div><Social icon={faTwitterSquare} href="tbc" /></div>
+					<div><Social icon={faTumblrSquare} href="tbc" /></div>
+					<div><button id="new-quote" onClick={this.handleClick}>New quote</button></div>
+				</div>
+			</React.Fragment>
+			);
+	}
+
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RandomQuoteGenerator />,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
