dbc4c51f95a515f50d2afe268440c61146bb671d
Your Name <you@example.com>
2020-06-04 19:42:33 +0100

    Updates

---
+++ precache-manifest.8f503399173fba440c1242ca4d3dbab5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c74f8203787f8b952255435182d476b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d302529954b130971ca",
+    "url": "/static/css/main.4fc4d29b.chunk.css"
+  },
+  {
+    "revision": "feae91f549343ade2ecd",
+    "url": "/static/js/2.ba201533.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba201533.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d302529954b130971ca",
+    "url": "/static/js/main.fea566ec.chunk.js"
+  },
+  {
+    "revision": "ebc8597b7d5c3e8ab5a4",
+    "url": "/static/js/runtime-main.f2e4871f.js"
+  }
+]);
